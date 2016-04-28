51bbc0f5ca720ebcd6abe1aa230dacf87b9f8cde
Tina Nguyen <tina@Tinas-MacBook-Pro.local>
2016-04-27 21:27:33 -0700

    Login Page frontend

--- app/src/main/java/com/spuds/eventapp/login_page.java
+++ app/src/main/java/com/spuds/eventapp/login_page.java
@@ -1,7 +1,11 @@
 package com.spuds.eventapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login_page extends AppCompatActivity {
 
@@ -9,5 +13,56 @@ public class login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        //a function to allow the user to sign in
+        signInFunc();
+
+    }
+
+    public void signInFunc(){
+        //create a button for the sign in
+        final Button signIn = (Button) findViewById(R.id.signIn);
+        //set a on click listener to see when the button is clicked
+        signIn.setOnClickListener(new View.OnClickListener(){
+            //what happens when the button is clicked
+            public void onClick(View v){
+                //get the user input for email field
+                String email = getEmail();
+                //get user input for password field
+                String password = getPassword();
+
+                //make sure that something is entered for email and password
+                if(email == "" || password == "" ){
+                    TextView errorMessage = (TextView)findViewById(R.id.errorMessage);
+                    errorMessage.setText("Missing fields. Please try again.");
+                }
+                //check if email is in database
+                /*else if(email ){
+                    TextView errorMessage = (TextView)findViewById(R.id.errorMessage);
+                    errorMessage.setText("The email you've entered doesn't match any account.
+                    Sign up for an account");
+                }
+                 */
+                //check if password is correct
+                /*else if(password){
+                    TextView errorMessage = (TextView)findViewById(R.id.errorMessage);
+                    errorMessage.setText("The password you've entered is incorrect");
+                }
+                 */
+                //email and password match and are correct
+                //Switch to the Main Activity
+                else{
+                    //startActivity(new Intent(login_page.this, MainActivity.class));
+                }
+            }
+        });
+    }
+    public String getEmail(){
+        EditText email = (EditText) findViewById(R.id.email);
+        return email.getText().toString();
+    }
+    public String getPassword() {
+        EditText password = (EditText) findViewById(R.id.password);
+        return password.getText().toString();
     }
 }
