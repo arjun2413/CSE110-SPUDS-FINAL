831ba740d3c6423103d63a7a9e3cb8e53bf224ba
Young Jin Yun <yngjnyn@gmail.com>
2016-05-04 23:00:08 -0700

    Further updated EditProfileFragment.java file. TODO: interface with Firebase, and implement OnClickListener.

--- app/src/main/java/com/spuds/eventapp/EditProfile/EditProfileFragment.java
+++ app/src/main/java/com/spuds/eventapp/EditProfile/EditProfileFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -15,6 +16,7 @@ import com.spuds.eventapp.R;
 public class EditProfileFragment extends Fragment {
 
     //interactable objects
+    Button updateButton;
     ImageButton editProfilePicture;
     EditText editFullName;
     EditText editCollege;
@@ -33,12 +35,32 @@ public class EditProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
+        updateButton = (Button) view.findViewById(R.id.update_button);
         editProfilePicture = (ImageButton) view.findViewById(R.id.edit_profile_picture);
         editFullName = (EditText) view.findViewById(R.id.edit_full_name);
         editCollege = (EditText) view.findViewById(R.id.edit_college);
         editMajor = (EditText) view.findViewById(R.id.edit_major);
         editDescription = (EditText) view.findViewById(R.id.edit_description);
 
+        /*TODO: here, we need to pull from the database the current information, and set the EditText hints to it.*/
+        //TODO: FIREBASE SHIT
+
+        editFullName.setHint("shit");
+        editCollege.setHint("shit");
+        editMajor.setHint("shit");
+        editDescription.setHint("shit");
+
+        //TODO: upon button clicklistener:
+        //Button listener for when user clicks submit changes
+        //get text for every edit text field and upon submit send to server
+        //TODO: push these strings to firebase and update.
+        editFullName.getText().toString();
+        editCollege.getText().toString();
+        editMajor.getText().toString();
+        editDescription.getText().toString();
+
+
+        //TODO: exit Edit Page and return to profile page.
         return view;
     }
 
