05abcd585abf1230e69a7ab3ffb95f71a55cd6cf
David Shan <sswdavid@gmail.com>
2016-05-21 16:53:04 -0700

    Added code to MainActivity that makes profile pic round, but it crashes when you re-run, so I commented it out. Changed Invite People cards.

--- app/src/main/java/com/spuds/eventapp/InvitePeople/InvitePeopleRVAdapter.java
+++ app/src/main/java/com/spuds/eventapp/InvitePeople/InvitePeopleRVAdapter.java
@@ -1,6 +1,10 @@
 package com.spuds.eventapp.InvitePeople;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -32,9 +36,9 @@ public class InvitePeopleRVAdapter extends RecyclerView.Adapter<InvitePeopleRVAd
             super(view);
 
             card = (CardView) view.findViewById(R.id.cv);
-            photo = (ImageView) view.findViewById(R.id.user_image);
-            inviteButton = (ImageView) view.findViewById(R.id.user_follow);
-            followerName = (TextView) view.findViewById(R.id.user_name);
+            photo = (ImageView) view.findViewById(R.id.invite_user_image);
+            inviteButton = (ImageView) view.findViewById(R.id.invite_toggle);
+            followerName = (TextView) view.findViewById(R.id.invite_user_name);
 
         }
     }
@@ -63,6 +67,13 @@ public class InvitePeopleRVAdapter extends RecyclerView.Adapter<InvitePeopleRVAd
         final int i = position;
         final InviteViewHolder inviteHolder = holder;
 
+        Bitmap src = BitmapFactory.decodeResource(fragment.getResources(), R.drawable.arjun);
+        RoundedBitmapDrawable dr =
+                RoundedBitmapDrawableFactory.create(fragment.getResources(), src);
+        dr.setCornerRadius(Math.max(src.getWidth(), src.getHeight()) / 2.0f);
+        holder.photo.setImageDrawable(dr);
+
+
         holder.inviteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,10 +87,11 @@ public class InvitePeopleRVAdapter extends RecyclerView.Adapter<InvitePeopleRVAd
             }
         });
 
+
         holder.followerName.setText(followers.get(position).name);
 
         /*
-        //What is selectAll?
+        //What is selectAll? How do you select all?
         if (selectAll)
             holder.scb.setChecked(true, true);
         else
