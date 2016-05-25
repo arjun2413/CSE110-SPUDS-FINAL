cfd54256502254b7e5a5f3cbad1c269504a78d96
Reggie <reggiewumo@gmail.com>
2016-05-24 21:43:05 -0700

    signup now checks email!

--- app/src/main/java/com/spuds/eventapp/Firebase/AccountFirebase.java
+++ app/src/main/java/com/spuds/eventapp/Firebase/AccountFirebase.java
@@ -145,10 +145,23 @@ public class AccountFirebase {
         String data = (String) ref.getAuth().getProviderData().get("email");
         return data;
     }
-    public void checkEmail(final String email, final TextView[] tArray) {
+
+    /*
+     * FUNCTION NAME: checkEmail
+     *
+     * PARAMETERS:
+     * String email - the email we are checking in the database
+     * TextView[] tArray - put your errorMessage textview into an array of size 1, and pass that in. If no error messages, just pass in null
+     * String error - the error message you want to print on failure
+     * boolean errorOnExist - pass true if the error is printed when email exists in database, false otherwise
+     *
+     * RESULT:
+     * threadCheck will set to 1 if found, and set to 2 if not found. call getThreadCheck() to check
+     */
+    public void checkEmail(final String email, final TextView[] tArray, final String error, final boolean errorOnExist) {
         threadCheck = 0;
         Firebase ref = new Firebase("https://eventory.firebaseio.com/users");
-        Query queryRef = ref.orderByChild("Email").startAt(email).endAt(email);
+        Query queryRef = ref.orderByChild("email").startAt(email).endAt(email);
         System.out.println(queryRef);
 //        Firebase ref = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
 //        Query queryRef = ref.orderByKey();
@@ -159,12 +172,15 @@ public class AccountFirebase {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     threadCheck = 1;
+                    if (tArray[0] != null && errorOnExist) {
+                        tArray[0].setText(error);
+                    }
                     System.out.println("FOUND");
                 }
                 else {
                     threadCheck = 2;
-                    if (tArray[0] != null) {
-                        tArray[0].setText("That email is not signed up");
+                    if (tArray[0] != null && !errorOnExist) {
+                        tArray[0].setText(error);
                     }
                     System.out.println("NOTFOUND");
                 }
