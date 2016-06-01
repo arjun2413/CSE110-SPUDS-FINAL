091fc635b1bb56d260bdf34c4de17a65890174b1
Karen <karenma333@gmail.com>
2016-05-31 17:50:47 -0700

    working on now

--- app/src/main/java/com/spuds/eventapp/Firebase/EventsFirebase.java
+++ app/src/main/java/com/spuds/eventapp/Firebase/EventsFirebase.java
@@ -470,7 +470,7 @@ public class EventsFirebase {
                     currentDate = df.format(dateobj);
 
 
-                    /*if (tabFilter != null && (tabFilter.equals(tabHot) || tabFilter.equals(tabNew))) {
+                    if (tabFilter != null && (tabFilter.equals(tabHot) || tabFilter.equals(tabNew))) {
                         if (currentDate.compareTo(newEvent.getDate()) < 0) {
                             eventsList.add(0, newEvent);
                         }
@@ -481,7 +481,7 @@ public class EventsFirebase {
                             eventsList.add(newEvent);
                         }
 
-                    }*/
+                    }
                     eventsList.add(0, newEvent);
                 }
 
